45eb14085f6777e66f7e132cf561d711e62bbada
nsaky <140412597+nsaky@users.noreply.github.com>
2023-10-13 12:50:40 +0530

    Major Changes

--- Landing Page/script.js
+++ Landing Page/script.js
@@ -1,3 +1,30 @@
+// REVIEWS
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  // adaptiveHeight: false,
+  dots: true,
+
+  autoplay: true,
+  autoplaySpeed: 3000,
+  autoplayHoverPause: false,
+
+  arrows: false,
+  container: true,
+  customPaging: function (index, a11ySlider) {
+    return '<button class="btn-pagination">' + index + "</button>";
+  },
+  responsive: {
+    481: {
+      dots: false,
+      slidesToShow: 2,
+    },
+    950: {
+      slidesToShow: null,
+    },
+  },
+});
+
+// PRICING
+
 const toggle = document.getElementById("price-toggle");
 const basicPrice = document.querySelector("#basic h1");
 const processionalPrice = document.querySelector("#professional h1");
